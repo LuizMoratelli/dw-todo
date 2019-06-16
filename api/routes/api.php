30630973e3f5e5c8c1e622f3fd2829af4216a1ec
Luiz Augusto Moratelli <gameplayteam12@gmail.com>
2019-06-16 11:05:18 -0300

    backend

---
+++ api/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::get('/todos', 'TodoController@index');
+Route::get('/todos/{id}', 'TodoController@show');
+Route::post('/todos', 'TodoController@store');
+Route::put('/todos/{id}', 'TodoController@update');
+Route::delete('/todos/{id}', 'TodoController@delete');
